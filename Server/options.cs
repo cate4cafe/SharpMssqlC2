a4d87b68480d68cc34f214d3cb64e59ba1f5e5f7
cate4cafe <39671538+cate4cafe@users.noreply.github.com>
2022-04-02 16:53:50 +0800

    Update options.cs

--- Server/options.cs
+++ Server/options.cs
@@ -10,7 +10,7 @@ namespace Server
         internal static string connStr = "Data Source={0};Initial Catalog={1};uid={2};pwd={3};MultipleActiveResultSets=true";
         internal static string host = "172.16.233.175";
         internal static string username = "sa";
-        internal static string password = "-pl,0okm";
+        internal static string password = "";
         internal static string dbname = "MssqlC2";
         internal static string table_server = "table_server";
         internal static string table_client = "table_client";
